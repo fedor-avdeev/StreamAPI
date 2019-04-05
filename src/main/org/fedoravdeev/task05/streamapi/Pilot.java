90bcf18c7f4d9a63d73ecc15d807707eb68d2d6d
Fedor Avdeev <fedor.avdeev@gmail.com>
2019-04-05 15:36:26 +0300

    added parse file

--- src/main/org/fedoravdeev/task05/streamapi/Pilot.java
+++ src/main/org/fedoravdeev/task05/streamapi/Pilot.java
@@ -3,28 +3,28 @@ package org.fedoravdeev.task05.streamapi;
 import java.time.Duration;
 
 public class Pilot {
-	final private String idPilot;
+	final private String pilotCode;
 	final private Duration bestTime;
-	final private String namePilot;
-	final private String team;
+	final private String pilotName;
+	final private String teamName;
 
-	public Pilot(String idPilot, Duration duration, String namePilot, String team) {
-		this.idPilot = idPilot;
-		this.bestTime = duration;
-		this.namePilot = namePilot;
-		this.team = team;
+	public Pilot(String pilotCode, Duration bestTime, String pilotName, String teamName) {
+		this.pilotCode = pilotCode;
+		this.bestTime = bestTime;
+		this.pilotName = pilotName;
+		this.teamName = teamName;
 	}
 
-	public String getIdPilot() {
-		return idPilot;
+	public String getPilotCode() {
+		return pilotCode;
 	}
 
-	public String getNamePilot() {
-		return namePilot;
+	public String getPilotName() {
+		return pilotName;
 	}
 
-	public String getTeam() {
-		return team;
+	public String getTeamName() {
+		return teamName;
 	}
 
 	public Duration getBestTime() {
--- src/main/org/fedoravdeev/task05/streamapi/Qualification.java
+++ src/main/org/fedoravdeev/task05/streamapi/Qualification.java
@@ -1,9 +1,5 @@
 package org.fedoravdeev.task05.streamapi;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.text.ParseException;
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -11,57 +7,49 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Observable;
 import java.util.concurrent.atomic.AtomicInteger;
-import static java.util.stream.Collectors.toList;
-
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Qualification {
-	private static final String WORKING_DIRECTORY = Paths.get("").toAbsolutePath().toString() + "/src/data/";
-	private static final String MINUS = "-";
+	
 	private static final int NUMBER_SYMBOLS_STRING = 72;
 	private static final int FIRST_FIFTEEN_PLACE = 15;
-	private final AtomicInteger count = new AtomicInteger();
-
+	private static final String LINE_SEPARATOR = "-";
+	private static final String DATE_FORMAT_QUALIFICATION = "yyyy-MM-dd_HH:mm:ss.SSS";
+	private static final String OUTPUT_FORMAT = "%-2d.%-17s | %-25s | %-11s\n";	
+	private final AtomicInteger count = new AtomicInteger();	
 	private List<Pilot> listPilotsRacing;
-
+	
 	public Qualification() {
 		this.listPilotsRacing = new ArrayList<>();
 	}
 
-	public void getResultQualification() throws ParseException {
-
+	public void getResultQualification() {
 		if (listPilotsRacing.isEmpty()) {
 			getListPilotsRacing();
 		}
-		
-		IntStream.iterate(1, (i) -> i + 1).peek(System.out::println).allMatch(i -> i < FIRST_FIFTEEN_PLACE);
-		//IntStream.iterate(1, n -> n + 1).limit(FIRST_FIFTEEN_PLACE).forEach(System.out.println(n)));
-		
+
 		listPilotsRacing.stream()
 			.sorted(Comparator.comparing(Pilot::getBestTime))
 			.limit(FIRST_FIFTEEN_PLACE)
-			.forEach(this::formatStringPilot);
+			.forEach(this::printLinePilot);		
 		listPilotsRacing.stream()
 			.limit(1)
-			.forEach(this::formatStringSeparator);
+			.forEach(this::printLineSeparator);
 		listPilotsRacing.stream()
 			.sorted(Comparator.comparing(Pilot::getBestTime))
 			.skip(FIRST_FIFTEEN_PLACE)
-			.forEach(this::formatStringPilot);
+			.forEach(this::printLinePilot);
 	}
 
-	private void formatStringSeparator(Pilot p) {
-		System.out.print(String.format(assemblyString(MINUS, NUMBER_SYMBOLS_STRING) + "\n"));
+	private void printLineSeparator(Pilot p) {
+		System.out.print(String.format(assemblyString(LINE_SEPARATOR, NUMBER_SYMBOLS_STRING) + "\n"));
 	}
 
-	private void formatStringPilot(Pilot p) {
-		System.out.print(String.format("%-2d.%-17s | %-25s | %-11s\n", 
-			count.incrementAndGet(), 
-			p.getNamePilot(),
-			p.getTeam(), 
+	private void printLinePilot(Pilot p) {		
+		System.out.print(String.format(OUTPUT_FORMAT,
+			count.incrementAndGet(),
+			p.getPilotName(),
+			p.getTeamName(), 
 			p.getStringBestTime()));
 	}
 
@@ -73,54 +61,44 @@ public class Qualification {
 		return stringSeparator.toString();
 	}
 
-	private List<Pilot> getListPilotsRacing() throws ParseException {
+	private List<Pilot> getListPilotsRacing() {
 
-		List<String> linesStart = getSortedLinesFromFile("start.log");
-		List<String> linesEnd = getSortedLinesFromFile("end.log");
-		List<String> listPilots = getSortedLinesFromFile("abbreviations.txt");
+		List<String> listStartTime = ParseFiles.getSortedLinesFromFile("start.log");
+		List<String> listEndTime   = ParseFiles.getSortedLinesFromFile("end.log");
+		List<String> listPilots    = ParseFiles.getSortedLinesFromFile("abbreviations.txt");
 
-		for (String stringStart : linesStart) {
-			listPilotsRacing.add(getPilot(stringStart, linesEnd, listPilots));
+		for (String lineStartTime : listStartTime) {
+			listPilotsRacing.add(getPilot(lineStartTime, listEndTime, listPilots));
 		}
 		return listPilotsRacing;
 	}
 
-	private List<String> getSortedLinesFromFile(String fileName) {
-		List<String> linesFromFile = new ArrayList<>();
-		Path pathFile = Paths.get(WORKING_DIRECTORY + fileName);
-		try (Stream<String> lineStreamFile = Files.newBufferedReader(pathFile).lines()) {
-			linesFromFile = lineStreamFile.sorted().collect(toList());
-		} catch (IOException ignored) {
-		}
-		return linesFromFile;
-	}
-
-	private Pilot getPilot(String stringTimeStart, List<String> listTimeEnd, List<String> listPilots) {
-		LocalDateTime startTime = getQualificationTime(stringTimeStart);
-		LocalDateTime endTime   = getQualificationTime(stringTimeStart);
+	private Pilot getPilot(String lineStartTime, List<String> listTimeEnd, List<String> listPilots) {
+		LocalDateTime startTime = getQualificationTime(lineStartTime);
+		LocalDateTime endTime   = getQualificationTime(lineStartTime);
 		String fullNamePilot = null;
-		String nameTeamLipot = null;
+		String nameTeamPilot = null;
 
 		for (String stringTimeEnd : listTimeEnd) {
-			if (stringTimeEnd.substring(0, 3).equals(stringTimeStart.substring(0, 3))) {
+			if (stringTimeEnd.substring(0, 3).equals(lineStartTime.substring(0, 3))) {
 				endTime = getQualificationTime(stringTimeEnd);
 				break;
 			}
 		}
 		
 		for (String pilot : listPilots) {
-			if (pilot.substring(0, 3).equals(stringTimeStart.substring(0, 3))) {
-				String[] nameTeamPilot = pilot.split("_");
-				fullNamePilot = nameTeamPilot[1];
-				nameTeamLipot = nameTeamPilot[2];
+			if (pilot.substring(0, 3).equals(lineStartTime.substring(0, 3))) {
+				String[] allNames = pilot.split("_");
+				fullNamePilot = allNames[1];
+				nameTeamPilot = allNames[2];
 				break;
 			}
 		}
-		return new Pilot(stringTimeStart.substring(0, 3), Duration.between(startTime, endTime), fullNamePilot, nameTeamLipot);
+		return new Pilot(lineStartTime.substring(0, 3), Duration.between(startTime, endTime), fullNamePilot, nameTeamPilot);
 	}
 
 	private LocalDateTime getQualificationTime(String lineStart) {
-		DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss.SSS");
+		DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern(DATE_FORMAT_QUALIFICATION);
 		return LocalDateTime.parse(lineStart.substring(3, 26),inputFormat);
 	}
 }
